55bf49dc16a5019e119697b3e1a241e3abb7f676
Jose Luis JoeDev <95258755+Jose890403@users.noreply.github.com>
2024-05-24 17:40:46 +0000

    segunda prueba pagina funcionando

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Post, Comment, Like, Suggestion, Dog
+from .models import db, User, Post, Comment, Like, Suggestion
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -16,7 +16,7 @@ def setup_admin(app):
     admin.add_view(ModelView(Comment, db.session))
     admin.add_view(ModelView(Like, db.session))
     admin.add_view(ModelView(Suggestion, db.session))
-    admin.add_view(ModelView(Dog, db.session))
+   
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
