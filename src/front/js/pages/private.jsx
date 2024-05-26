cf1d9ee488e32f2decf402f5fffc51ad6c414cf1
Maikel Guerra Pantoja <154547587+Maikgp@users.noreply.github.com>
2024-05-26 02:40:11 +0000

    casi listo

--- src/front/js/pages/private.jsx
+++ src/front/js/pages/private.jsx
@@ -2,47 +2,51 @@ import React, { useState, useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/private.css";
 
-
 export const Private = () => {
 	const { store, actions } = useContext(Context);
 	const [img, setImg] = useState("");
-	const [bodytext, setBodytext] = useState("")
+	const [bodytext, setBodytext] = useState("");
 
 	const handleSubmit = e => {
 		e.preventDefault();
 		actions.createPost(img, bodytext);
-		setImg(" ");
-		setBodytext(" ");
-	}
+		setImg("");
+		setBodytext("");
+	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<div className="Content-img">
-				<label>
-					Photo
-					{img && <img src={img} alt="Uploaded" style={{ width: "300px", height: "300px" }} />}
-					<input
-						type="text"
-						name="img"
-						value={img}
-						onChange={e => setImg(e.target.value)}
-					/>
-
-				</label>
+		<form className="post-form" onSubmit={handleSubmit}>
+			<div className="form-group">
+				<label className="form-label">Insert URL</label>
+				<input
+					type="text"
+					name="img"
+					value={img}
+					onChange={e => setImg(e.target.value)}
+					className="form-input"
+				/>
 			</div>
 
-			<div>
-				<label>
-					Body-Text
-					<input
-						type="text"
-						name="bodytext"
-						value={bodytext}
-						onChange={e => setBodytext(e.target.value)}
-					/>
-				</label>
+			<div className="form-group">
+				<label className="form-label">Comment</label>
+				<input
+					type="text"
+					name="bodytext"
+					value={bodytext}
+					onChange={e => setBodytext(e.target.value)}
+					className="form-input"
+				/>
 			</div>
-			<button type="submit">Enviar</button>
+
+			{img && (
+				<div className="image-preview">
+					<img src={img} alt="Uploaded" className="preview-img" />
+				</div>
+			)}
+
+			<div className="button-container">
+				<button type="submit" className="submit-button">Send Post</button>
+			</div>
 		</form>
 	);
 };
